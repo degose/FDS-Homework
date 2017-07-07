900fa126501d43a61a00f61753892f5cc4cc27bb
gose <gom0367@gmail.com>
2017-07-07 19:32:49 +0900

    백업

--- groupTodo.1/public/js/grouptodo.js
+++ groupTodo.1/public/js/grouptodo.js
@@ -9,18 +9,20 @@
   'use strict';
 
   // 변수 설정들
-  var app, todo ,todo_buttons, todo_title, todo_content, todo_items, todo_storage, template, todo_item, target, remove_id;
+  var app, todo ,todo_buttons, todo_title, todo_content, todo_items, todo_storage, template, todo_item, target, remove_id, select, todo_items_1, todo_items_2, todo_items_3, value, e, value_1,value_2,value_3;
 
   var document = global.document;
   var todo_api_address = '';
   var forEach = Array.prototype.forEach;
 
 
-  // 변수 몰아주기
   function init() {
 
+
+
     // 메모 데이타 주소
-    todo_api_address = '../toDo'; // html입장에서 주소 경로
+    var e = document.getElementById('user').options[document.getElementById('user').selectedIndex].text;
+        todo_api_address = "/" + e;   // << 데이터 저장되는 곳
 
     // html 요소 가져오기
     app = document.querySelector('.app');
@@ -28,35 +30,42 @@
     todo_buttons = document.querySelectorAll('button')
     todo_title = document.querySelector('#todo-title');
     todo_content = document.querySelector('#todo-content');
-    todo_items = document.querySelector('.todo-item-container'); // todo 목록들
-    // console.log(todo_items);
+    // todo_items = document.querySelector('.todo-item-container'); // todo 목록들 // ev.target주려면 있어야함
+    todo_items_1 = document.querySelector('.todo-items-1');
+    todo_items_2 = document.querySelector('.todo-items-2');
+    todo_items_3 = document.querySelector('.todo-items-3');
+    // todo_items2 = document.querySelector('.todo-item-container2'); // todo 목록들
+    // todo_items3 = document.querySelector('.todo-item-container3'); // todo 목록들
+    select = document.querySelector('select');
+    // value = select.value;
+    // console.log("value",value);
+
+    
+    
 
     // 데이터 가져오는 함수
     load();
 
     // 이벤트 걸어주는 함수
     bind();
-
-    // // todo 아이템 보여지는 함수
-    // render(); // 렌더를 여기서 말고 load에서
     
   }
-
 
   // 데이터 통신
   // 데이터 가져오기
   function load(params) {
 
+
     // toDo.json에서 데에터 가져오기(GET)
     $.get(todo_api_address, function (data, status) {
 
       // 통신 연결 상태 확인
-      console.log('통신 상태:', status); // status
-      console.log('데이터:', data); // []
+      // console.log('통신 상태:', status); // status
+      // console.log('데이터:', data); // []
       
       // toDo.json에 데이터를 storage에 참조
       todo_storage = data;
-      console.log(todo_storage);
+      // console.log("todo_storage",todo_storage);
 
       // 데이터를 가져온 후 렌더링 해준다.
       render();
@@ -72,7 +81,9 @@
     });
 
     // todo아이템에 삭제 이벤트 
-    todo_items.addEventListener('click', removeTodo);
+    todo_items_1.addEventListener('click', removeTodo);
+    todo_items_2.addEventListener('click', removeTodo);
+    todo_items_3.addEventListener('click', removeTodo);
     // 버튼 자체가 아니라 감싸고있는 컨테이너에 이벤트를 줬다. => 이유는 계속해서 생성될 todo아이템들에 계속해서 이벤트를 걸어줄 수 없으니 부모 요소에 넣어주고 캡처링을 이용하기 위해서. 
   }
 
@@ -101,11 +112,16 @@
     if(validateTodo(todo_title, todo_content)){return;}
     // todo_item에 value값 불러오기
     todo_item = {
+
       title: todo_title.value,
       content: todo_content.value
     };
 
     // 서버에 올리기(POST)
+    select = document.querySelector('select');
+    value = select.value;
+    var e = document.getElementById('user').options[document.getElementById('user').selectedIndex].text;
+        todo_api_address = "/" + e;   // << 데이터 저장되는 곳
     $.post(todo_api_address, $.param(todo_item), function (data, status) {
       // .param 왜 해줬지? => 배열이나 객체를 string으로 바꿔줌( 왜냐하면 .post의 인자로 (api,string,콜백함수)가 와야해서)
       // 서버에 올린 후 가져오기(GET)
@@ -145,15 +161,16 @@
 
   // todo 아이템 보이기
   function render() {
+    
 
     // 왜 빈 문자열로 초기화? 초기화 안해주면 화면에 undefined나옴 => 뒤에 문자열 더해줄거라서 문자열이라고 말해주는것
     template = '';
     // todo_items에 태그 넣기
-    todo_items.innerHTML = '';
+    // todo_items.innerHTML = '';
     // todo_storage에 목록들을 순환해서 tempate에 추가
     todo_storage.forEach(function (todo) {
       template += 
-      '<article class="todo-item column is-3 message is-primary">'+
+      '<article class="todo-item column message is-info">'+
         '<div class="message-header">'+
           '<h5 class="todo-item-title">'+todo.title+'</h5>'+
           '<button data-remove-index="'+todo.id+'" type="button" class="delete" aria-label="할일 아이템 제거"></button>'+
@@ -163,9 +180,39 @@
         '</div>'+
       '</article>'
     });
+    // todo_items_3.innerHTML = template;
     // todo_items에 태그 넣기
-    todo_items.innerHTML = template;
-  }
+
+    // function interA() {
+    //   todo_items_1.innerHTML = template;
+    // };
+    // interA();
+    // function interB() {
+    //   todo_items_2.innerHTML = template;
+    // };
+    // interB();
+    // function interC() {
+    //   todo_items_3.innerHTML = template;
+    // };
+    // interC();
+
+
+
+
+
+    if ( value === "user1") {
+      todo_items_1.innerHTML = template;
+    }
+    else if (value === "user2") {
+      todo_items_2.innerHTML = template;
+    }
+    else if (value === "user3") {
+      todo_items_3.innerHTML = template;
+    }
+
+
+    console.log("select.value:",value);
+  };
 
 
   // init 함수 실행
